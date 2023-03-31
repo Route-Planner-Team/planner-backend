9e69afa8dd0435ab968238a496dbb48b8d60733a
Yurii <yurbor@st.amu.edu.pl>
2023-03-31 16:20:54 +0200

    calculate simple road

--- route/planner.py
+++ route/planner.py
@@ -1,5 +1,7 @@
 import requests
 import polyline
+import logging
+from loguru import logger
 from config import Config
 
 class RoutePlanner():
@@ -15,3 +17,34 @@ class RoutePlanner():
         #     "1 Hacker Way, Menlo Park, CA",
         #     "350 Rhode Island St, San Francisco, CA"
         # ]
+
+        response = requests.get(
+            "https://maps.googleapis.com/maps/api/directions/json",
+            params={
+                "origin": addresses[0],
+                "destination": addresses[-1],
+                "key": self.config.GOOGLEMAPS_API_KEY
+            }
+        )
+
+        if response.status_code != 200:
+            return {"error": "Failed to calculate route"}
+
+        data = response.json()
+        # print(data)
+
+        points = []
+
+        steps = data['routes'][0]['legs'][0]['steps']
+
+        tmp_array = []
+        for step in steps:
+            polyline_str = step['polyline']['points']
+            tmp_array.append(polyline_str)
+
+            decoded_polyline = polyline.decode(polyline_str)
+
+
+            points.extend(decoded_polyline)
+
+        return {"route": points}
