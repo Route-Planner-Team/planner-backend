bf2e8fb8d293eafc939dffb5db3fcd6abe58adf2
aliaksei-kudravets <frume28@gmail.com>
2023-05-20 17:33:25 +0200

    repair test, add protected user_route endpoint

--- routes/conftest.py
+++ routes/conftest.py
@@ -1,8 +1,10 @@
 import pytest
 from main import routes_repo
+from firebase_admin import auth
 
 def pytest_sessionfinish(session, exitstatus):
     # This code will be executed as teardown after all tests
     # Put your teardown code here
     print("Teardown after all tests")
-    routes_repo.delete_user_route(email="test_user@gmail.com")
+    # firebase_user = auth.get_user(uid)
+    routes_repo.delete_user_route(uid="uub8aAbFW3QFOAi49ql98KfHQIA2")
